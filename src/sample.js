cbc6ff55937c991b235217109c91d95eca8cd0b6
kamal saluja <saluja_kamal2000@yahoo.com>
2019-10-12 19:08:36 +1100

    rename function

--- src/sample.js
+++ src/sample.js
@@ -1,9 +1,9 @@
 const first = {
-	test: function(va) {
+	simpleFunction: function(va) {
 		console.log('hello' + va)
 	}
 }
 
-first.test('123')
+first.simpleFunction('123')
 
 //this is sample program to display parameter....
