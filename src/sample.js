8fc85fc1cb2b4b6243f010e9710232b55ded3a53
kamal saluja <saluja_kamal2000@yahoo.com>
2019-10-12 19:20:55 +1100

    after soft reset

--- src/sample.js
+++ src/sample.js
@@ -4,7 +4,14 @@ const first = {
 	},
 	simpleFunction2: function(va) {
 		console.log('hello' + va)
+	},
+	simpleFunction3: function(va) {
+		console.log('hello' + va)
+	},
+	simpleFunction4: function(va) {
+		console.log('hello' + va)
 	}
+
 }
 
 first.simpleFunction1('12345')
